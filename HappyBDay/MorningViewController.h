90c6f1e9ae9e22e11e83b9664853bf03605e00b7
BandoKal <jason.bandy77@gmail.com>
2015-03-31 22:35:57 -0500

    Adding the morning controller... JIRA-12

---
+++ HappyBDay/MorningViewController.h
@@ -0,0 +1,13 @@
+//
+//  MorningViewController.h
+//  HappyBDay
+//
+//  Created by Jason on 3/31/15.
+//  Copyright (c) 2015 BandoKal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MorningViewController : UIViewController
+
+@end
