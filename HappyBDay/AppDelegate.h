5cb0bc38c0bf8cb8ff634d0fd587a299e7771162
BandoKal <jason.bandy77@gmail.com>
2015-03-31 21:07:50 -0500

    Initial commit

---
+++ HappyBDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyBDay
+//
+//  Created by Jason on 3/31/15.
+//  Copyright (c) 2015 BandoKal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyBDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HappyBDay
+//
+//  Created by Jason on 3/31/15.
+//  Copyright (c) 2015 BandoKal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
